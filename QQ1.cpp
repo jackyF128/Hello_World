32df781374f8967f421a738fe89b117c9d4e18e4
Jacky <jackyF128@hotmail.com.tw>
2015-03-11 10:15:26 +0800

    我會做了!!!

---
+++ QQ1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	
+	
+	prinf('%d\n, 2+3');
+	return 123;
+	
+}
---
+++ 新文件2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char Name[50] ="QAQ";
+	printf("Hello World!\n");
+	printf("%d\n", 2+3);
+	printf("%d\n", 1, 2+3,654);
+	printf("%d\n", cos(2.0*M_PT/2,3.0));
+	return 123;
+	
+}
