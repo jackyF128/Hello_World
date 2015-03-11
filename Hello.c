ef262757d0340b7892e6a85ab6cd87c256b19533
Jacky <jackyF128@hotmail.com.tw>
2015-03-11 11:09:26 +0800

    2015/3/11 上課內容

--- Hello.c
+++ Hello.c
@@ -2,13 +2,18 @@
 
 int main()
 {
-	char Name[50] ="QAQ";
+    int i;
+	char Name[50] ="jackyF128 haha";
 	printf("Hello World!\n");
 	printf("%d\n", 2+3);
 	printf("%d\n", 1, 2+3,654);
 	printf("%s\n", Name);
 	printf("%c\n", Name[5]);
 	printf("%s\n", Name+3);
-	return 123;
-	
+	printf("%u\n", Name);
+	for(i=0;i<50;++i)
+	   {
+		printf("%d :%c-%u\n",i, Name[i], Name[i]);
+	}
+	return 123;	
 }
